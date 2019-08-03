c58fb0ce7b353591115d9993a3f6fed552f4289b
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-03 23:26:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,12 +3,13 @@ require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
   get '/' do
+    
     erb :index
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+   
     erb :results
   end
 end
